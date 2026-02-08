70aaf4ce7e64b23a0061aa2c1484d04ca628ba39
cgl <cgldtsci@163.com>
2026-02-08 23:33:54 +0800

    convert sft data

--- deal_dataset.py
+++ deal_dataset.py
@@ -29,6 +29,39 @@ def deal_pretain_data(input_path):
                     pretrain.write(json.dumps({'text': chunk}, ensure_ascii=False) + '\n')
 
 
+def convert_message(data):
+    """
+    将原始数据转换为标准格式
+    """
+    message = [
+        {"role": "system", "content": "你是一个AI助手"},
+    ]
+    for item in data:
+        if item['from'] == 'human':
+            message.append({'role': 'user', 'content': item['value']})
+        elif item['from'] == 'assistant':
+            message.append({'role': 'assistant', 'content': item['value']})
+    return message
+
+
+def deal_sft_data(input_path):
+
+    input_path = Path(input_path).resolve()
+    output_name = 'sft_' + input_path.name
+
+    parent = input_path.parent
+
+    output_sft_path = parent / output_name
+
+    with open(output_sft_path, 'w', encoding='utf-8') as sft:
+        with open(input_path, 'r', encoding='utf-8') as f:
+            data = f.readlines()
+            for item in tqdm(data, desc="Processing", unit="lines"):
+                item = json.loads(item)
+                message = convert_message(item['conversations'])
+                sft.write(json.dumps(message, ensure_ascii=False) + '\n')
+
+
 if __name__ == "__main__":
     # ==================== 命令行参数解析 ====================
     parser = argparse.ArgumentParser(description="deal dataset")
@@ -40,3 +73,5 @@ if __name__ == "__main__":
 
     if args.train_mode == 'pretrain':
         deal_pretain_data(args.data_path)
+    elif args.train_mode == 'sft':
+        deal_sft_data(args.data_path)
