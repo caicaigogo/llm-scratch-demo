3b6d8d667977f104291df09c5b38e402d4d46d22
caicaigogo <cgl1991fzu@sina.com>
2026-02-09 16:25:13 +0800

    add get tokenizer component

--- src/llm_scratch/train_component/argument.py
+++ src/llm_scratch/train_component/argument.py
@@ -9,7 +9,7 @@ class CustomizedArguments:
     """
     max_seq_len: int = field(metadata={"help": "输入最大长度"})
     # train_file: str = field(metadata={"help": "训练集。如果task_type=pretrain，请指定文件夹，将扫描其下面的所有jsonl文件"})
-    # model_name_or_path: str = field(metadata={"help": "预训练权重路径"})
+    model_name_or_path: str = field(metadata={"help": "预训练权重路径"})
     # template_name: str = field(default="", metadata={"help": "sft时的数据格式"})
     # eval_file: Optional[str] = field(default="", metadata={"help": "验证集"})
     # max_prompt_length: int = field(default=512, metadata={"help": "dpo时，prompt的最大长度"})
--- trainer_train.py
+++ trainer_train.py
@@ -6,7 +6,9 @@ from loguru import logger
 from transformers import (
     HfArgumentParser,
     TrainingArguments,
-    set_seed
+    set_seed,
+    AutoTokenizer,
+    AutoConfig
 )
 
 from llm_scratch.train_component.argument import CustomizedArguments
@@ -46,10 +48,68 @@ def setup_everything():
     return cus_args, training_args
 
 
+def load_tokenizer(args):
+
+    # ModelConfig {
+    #   "architectures": [
+    #     "LLaMAForCausalLM"
+    #   ],
+    #   "auto_map": {
+    #     "AutoConfig": "k_model.ModelConfig",
+    #     "AutoModelForCausalLM": "k_model.LLaMAForCausalLM"
+    #   },
+    #   "dtype": "float32",
+    #   "hidden_size": 512,
+    #   "intermediate_size": 3072,
+    #   "max_position_embeddings": 512,
+    #   "max_seq_len": 512,
+    #   "model_type": "Tiny-K",
+    #   "num_attention_heads": 16,
+    #   "num_hidden_layers": 2,
+    #   "num_key_value_heads": 8,
+    #   "rms_norm_eps": 1e-05,
+    #   "rope_theta": 10000.0,
+    #   "transformers_version": "4.57.6",
+    #   "vocab_size": 6144
+    # }
+    config = AutoConfig.from_pretrained(args.model_name_or_path, trust_remote_code=True)
+    print(config)
+    # 加载tokenzier
+    # 从本地路径加载预训练的分词器
+    # PreTrainedTokenizerFast(name_or_path='k-model-215M', vocab_size=6144, model_max_length=1000000000000000019884624838656, is_fast=True, padding_side='right', truncation_side='right', special_tokens={'bos_token': '<|im_start|>', '
+    # eos_token': '<|im_end|>', 'unk_token': '<unk>', 'pad_token': '<|im_end|>', 'additional_special_tokens': ['<s>', '</s>']}, clean_up_tokenization_spaces=False, added_tokens_decoder={
+    #         0: AddedToken("<unk>", rstrip=False, lstrip=False, single_word=False, normalized=False, special=True),
+    #         1: AddedToken("<s>", rstrip=False, lstrip=False, single_word=False, normalized=False, special=True),
+    #         2: AddedToken("</s>", rstrip=False, lstrip=False, single_word=False, normalized=False, special=True),
+    #         3: AddedToken("<|im_start|>", rstrip=False, lstrip=False, single_word=False, normalized=False, special=True),
+    #         4: AddedToken("<|im_end|>", rstrip=False, lstrip=False, single_word=False, normalized=False, special=True),
+    # }
+    # )
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)
+    print(tokenizer)
+    assert tokenizer.pad_token_id is not None, "pad_token_id should not be None"
+    assert tokenizer.eos_token_id is not None, "eos_token_id should not be None"
+    logger.info(f'vocab_size of tokenizer: {tokenizer.vocab_size}')
+
+    return tokenizer
+
+
+def init_components(cus_args, training_args):
+    """
+    初始化各个组件
+    """
+    training_args.ddp_find_unused_parameters = False
+    logger.info('Initializing components...')
+
+    # 加载tokenizer
+    tokenizer = load_tokenizer(cus_args)
+
 def main():
     # 进行一些配置和检查
     cus_args, training_args = setup_everything()
 
+    # 加载各种组件
+    trainer = init_components(cus_args, training_args)
 
 if __name__ == "__main__":
     main()
